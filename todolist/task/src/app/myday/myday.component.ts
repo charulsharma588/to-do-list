ccfcdb696f3a803aad3e3c0b290e08e9ab6e2a8a
charulsharma588 <charulsharma588@gmail.com>
2020-09-05 19:59:48 +0530

    fixed bugs

--- todolist/task/src/app/myday/myday.component.ts
+++ todolist/task/src/app/myday/myday.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit ,Output,EventEmitter} from '@angular/core';
 export class MydayComponent implements OnInit {
 
 
+  now = new Date();
 
   constructor() { }
   currentv=<any>[];
@@ -19,7 +20,29 @@ export class MydayComponent implements OnInit {
   ngOnInit() {
     this.currentv[this.i]=JSON.parse(localStorage.getItem("My tasks"));
   }
- 
+  checking=<any>[];
+
+  check(y){
+       
+   
+      this.checking[this.i]=y;
+     
+      localStorage.setItem("completed tasks",JSON.stringify(this.checking));
+     
+      this.i=this.i+1;
+     
+    
+    
+  
+  }
+  
+  markig=<any>[];
+  marked(x){
+    this.markig[this.i]=x;
+    localStorage.setItem("important tasks",JSON.stringify(this.markig));
+    this.i=this.i+1;
+    
+  }
 
 
 }
